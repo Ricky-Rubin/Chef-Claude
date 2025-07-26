b5d7fa04d006210dbc40f139e96b5363279b4455
Owolabi <owolabi1738@gmail.com>
2025-07-26 12:10:28 +0100

    Rendered the Header component in the main vai the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
+import { Header } from "/src/Header.jsx"
 
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export { App };
--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,7 @@
+import { createRoot } from "react-dom/client"
+import { App } from "/src/App.jsx"
 
+const theRender = createRoot(document.getElementById('root'));
+theRender.render(
+  <App />
+);
