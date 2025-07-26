29c45c9fc4141d899b9aa0660c3dee1825413503
Owolabi <owolabi1738@gmail.com>
2025-07-26 12:06:03 +0100

    Created header component

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import { chef_logo } from "/chef-claude-icon.png"
+
+function Header() {
+    return (
+        <div className="header">
+            <img src={chef_logo}/>
+            <span className="header-text">Chef Claude</span>
+        </div>
+    )
+}
+
+export { Header };
