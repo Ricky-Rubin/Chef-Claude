63e6305b863349a3347ae6ee25b16889947b9f45
Owolabi <owolabi1738@gmail.com>
2025-07-26 13:18:10 +0100

    Created a form component for the input and button segment of the page. Updated styling for fit to design specs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "/src/Header.jsx"
+import { Form } from "/src/Form.jsx"
 
 function App() {
   return (
     <>
       <Header />
+      <Form />
     </>
   )
 }
---
+++ src/Form.jsx
@@ -0,0 +1,10 @@
+function Form() {
+    return (
+        <form>
+            <input type="text" className="text-bar" placeholder="e.g. oregano"/>
+            <button className="form-button">+ Add Ingredient</button>
+        </form>
+    )
+}
+
+export { Form };
