ec968ea8d3ec6a4b3ab3b554baaaddb82de5a09b
Owolabi <owolabi1738@gmail.com>
2025-07-27 21:41:19 +0100

    Updated some styling and implemented event listeners and addition of elements to array within react

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,31 @@
 function Form() {
+    const array = ['Top', 'gun'];
+
+    function logSomething(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const addedIngredient = formData.get("ingredient");
+        array.push(addedIngredient);
+        console.log(array);
+
+        /*
+            const check = document.getElementById("text-bar");
+            array.push(check.value)
+            console.log(array);
+
+            While the lines above did the exact thing I wanted,
+            I guess that is the JS method since working with JavaScript
+            is imperative and not declarative like react. 
+
+            The new FormData, event.currentTarget and the get method
+            which took the name attrubute of the input element seems to 
+            be a method that works well with react STATE. 
+        */
+    }
+
     return (
-        <form>
-            <input type="text" className="text-bar" placeholder="e.g. oregano"/>
+        <form onSubmit={logSomething}>
+            <input name="ingredient" type="text" id="text-bar" className="text-bar" placeholder="e.g. oregano"/>
             <button className="form-button">Add Ingredient</button>
         </form>
     )
