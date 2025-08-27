a2019243dff44f90c6f3ae445b92b3b41cf5f5e6
Owolabi <owolabi1738@gmail.com>
2025-08-28 00:04:44 +0100

    Imported a Markdown Library to handle AI response

--- src/ClaudeRecipe.jsx
+++ src/ClaudeRecipe.jsx
@@ -1,9 +1,11 @@
+import ReactMarkdown from "react-markdown"
+
 function ClaudeRecipe(props) {
     return (
         <section className="feedback">
             <h2>Chef Claude Recommends:</h2>
             <article className="suggested-recipe-container" aria-live="polite">
-                <div>{props.fromAi}</div>
+                <ReactMarkdown>{props.fromAi}</ReactMarkdown>
             </article>
         </section>
     )
